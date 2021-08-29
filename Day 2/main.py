2ec1c1dc9a724eaa07df5d2d0d36d4c4c09774de
Sethu Narayan Senthil <github@sethusenthil.com>
2021-08-29 19:26:46 -0400

    day 2

---
+++ Day 2/main.py
@@ -0,0 +1,7 @@
+user_name = input("Please enter your name:  ")
+#print('Hi ' + user_name + "!")
+print(f"Hi {user_name}!"); #string formating
+
+year = input("Please enter what year you were born:  ")
+print(f"In the year 2025 you'll be {2025 - int(year)}!"); #string formating
+#str() converts string
